bd57f63611f5074d3eb271d7913cf0c9b20656eb
LasseAafeldt <superlla@hotmail.com>
2020-04-18 00:40:44 +0200

    made an item spawner
    Spawn items randomly in the available spawn points. Items to spawn are not selected in any way yet (currently just spawns the same item alot of times).

---
+++ NGJO 2020/Assets/Scripts/ItemSpawnPositions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnPositions : MonoBehaviour
+{
+    [SerializeField] private List<Transform> SpawnPositions;
+
+    private void Awake()
+    {
+        SpawnPositions = new List<Transform>();
+        PopulateList(SpawnPositions);
+    }
+
+    void PopulateList(List<Transform> childTrans)
+    {
+        Transform[] ch;
+        ch = gameObject.GetComponentsInChildren<Transform>();
+
+        for (int i = 1; i < ch.Length; i++) //i is = 1 because GetComponentsInChildren returns the compenent in the parent as index 0
+        {
+            childTrans.Add(ch[i]);
+        }
+    }
+
+    public List<Transform> GetAllSpawnPositions()
+    {
+        return SpawnPositions;
+    }
+}
---
+++ NGJO 2020/Assets/Scripts/SpawnItemsInAvailiblePosition.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItemsInAvailiblePosition : MonoBehaviour
+{
+    [SerializeField] private int NumItemsToSpawn = 10;
+    [SerializeField] private GameObject itemToSpawn;
+
+    ItemSpawnPositions spawnPoints;
+
+    public List<Transform> availableSpawnPoints;
+
+    private void Awake()
+    {
+        spawnPoints = GetComponent<ItemSpawnPositions>();
+    }
+
+    private void Start()
+    {
+        availableSpawnPoints = new List<Transform>(spawnPoints.GetAllSpawnPositions());
+        SpawnItems();
+    }
+
+    void SpawnItems()
+    {
+        for (int i = 0; i < NumItemsToSpawn; i++)
+        {
+            Vector3 placeToSpawn = SelectAvailableSpawnPoint().position;
+            Instantiate(itemToSpawn, placeToSpawn, Quaternion.identity);
+        }
+    }
+
+    Transform SelectAvailableSpawnPoint()
+    {
+        //Debug.Log("Selecting...");
+        Transform chosenTransform;
+        int index = Random.Range(0, availableSpawnPoints.Count);
+
+        chosenTransform = availableSpawnPoints[index];
+        //Debug.Log("Removed: " + chosenTransform);
+        availableSpawnPoints.Remove(chosenTransform);
+
+        return chosenTransform;
+    }
+}
