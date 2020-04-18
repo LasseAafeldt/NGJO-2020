a493d72ef193fae0cba064f957cc5bdc66a15636
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2020-04-18 19:49:17 +0200

    Spawn random reagent.

--- NGJO 2020/Assets/Scripts/SpawnItemsInAvailiblePosition.cs
+++ NGJO 2020/Assets/Scripts/SpawnItemsInAvailiblePosition.cs
@@ -2,45 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnItemsInAvailiblePosition : MonoBehaviour
-{
-    [SerializeField] private int NumItemsToSpawn = 10;
-    [SerializeField] private GameObject itemToSpawn;
+public class SpawnItemsInAvailiblePosition : MonoBehaviour {
+	[SerializeField] private int NumItemsToSpawn = 10;
+	[SerializeField] private GameObject[] itemsToSpawn;
 
-    ItemSpawnPositions spawnPoints;
+	ItemSpawnPositions spawnPoints;
 
-    public List<Transform> availableSpawnPoints;
+	public List<Transform> availableSpawnPoints;
 
-    private void Awake()
-    {
-        spawnPoints = GetComponent<ItemSpawnPositions>();
-    }
+	private void Awake() {
+		spawnPoints = GetComponent<ItemSpawnPositions>();
+	}
 
-    private void Start()
-    {
-        availableSpawnPoints = new List<Transform>(spawnPoints.GetAllSpawnPositions());
-        SpawnItems();
-    }
+	private void Start() {
+		availableSpawnPoints = new List<Transform>(spawnPoints.GetAllSpawnPositions());
+		SpawnItems();
+	}
 
-    void SpawnItems()
-    {
-        for (int i = 0; i < NumItemsToSpawn; i++)
-        {
-            Vector3 placeToSpawn = SelectAvailableSpawnPoint().position;
-            Instantiate(itemToSpawn, placeToSpawn, Quaternion.identity);
-        }
-    }
+	void SpawnItems() {
+		for (int i = 0; i < NumItemsToSpawn; i++) {
+			Vector3 placeToSpawn = SelectAvailableSpawnPoint().position;
+			Instantiate(itemsToSpawn[Random.Range(0, itemsToSpawn.Length)], placeToSpawn, Quaternion.identity);
+		}
+	}
 
-    Transform SelectAvailableSpawnPoint()
-    {
-        //Debug.Log("Selecting...");
-        Transform chosenTransform;
-        int index = Random.Range(0, availableSpawnPoints.Count);
+	Transform SelectAvailableSpawnPoint() {
+		//Debug.Log("Selecting...");
+		Transform chosenTransform;
+		int index = Random.Range(0, availableSpawnPoints.Count);
 
-        chosenTransform = availableSpawnPoints[index];
-        //Debug.Log("Removed: " + chosenTransform);
-        availableSpawnPoints.Remove(chosenTransform);
+		chosenTransform = availableSpawnPoints[index];
+		//Debug.Log("Removed: " + chosenTransform);
+		availableSpawnPoints.Remove(chosenTransform);
 
-        return chosenTransform;
-    }
+		return chosenTransform;
+	}
 }
