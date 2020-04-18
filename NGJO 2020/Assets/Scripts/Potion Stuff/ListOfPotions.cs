e0c218524510dc771157953820ebc1648d1d6bd0
LasseAafeldt <superlla@hotmail.com>
2020-04-18 14:49:04 +0200

    lots of potion stuff.
    Tried to make functionallity for the Potion UI

---
+++ NGJO 2020/Assets/Scripts/Potion Stuff/ListOfPotions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListOfPotions : MonoBehaviour
+{
+    static ListOfPotions _instance;
+    public static ListOfPotions instance
+    {
+        get { return _instance; }
+    }
+
+
+    public PotionScriptableObject charismaPotion;
+    //some other potions
+
+    private PotionScriptableObject potionToCraft;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public PotionScriptableObject matchPotionWithSelected(PotionScriptableObject selectedPotion)
+    {
+        if (selectedPotion.Equals(charismaPotion))
+        {
+            return charismaPotion;
+        }
+
+        return null;
+    }
+
+    public void SetPotionToCraft(PotionScriptableObject potion)
+    {
+        potionToCraft = potion;
+    }
+}
---
+++ NGJO 2020/Assets/Scripts/Potion Stuff/PotionScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Potion", menuName = "Create new Potion")]
+public class PotionScriptableObject : ScriptableObject
+{
+    public string _name = "New Potion";
+    [TextArea]
+    public string description;
+    public Sprite sprite;
+
+    public List<IngredientScriptableObject> ingredients;
+    public List<int> amountOfIngredients;
+
+    public int amountOfPotion;
+
+}
