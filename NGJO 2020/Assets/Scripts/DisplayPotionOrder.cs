d52d92206a24fb9ae9e300d24031fcc9e1636636
LasseAafeldt <superlla@hotmail.com>
2020-04-18 10:41:19 +0200

    Made simple dialogue display + added TMPro

---
+++ NGJO 2020/Assets/Scripts/DisplayPotionOrder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayPotionOrder : MonoBehaviour
+{
+    public string potionOrder = "Default Potion"; //change when we know how potions are done
+
+    [SerializeField] private string[] order;
+    [SerializeField] private string[] reason;
+
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        DisplayOrder();
+    }
+
+    void DisplayOrder()
+    {
+        potionOrder = "Default Potion"; //do some kind of get potion thing when we know how potions are done
+
+        string _order = SelectOrderPhrasing();
+        string _reason = SelectReasonPhrasing();
+
+        text.text = _order + " " + potionOrder + " " + _reason;
+    }
+
+    string SelectOrderPhrasing()
+    {
+        int index = Random.Range(0, order.Length - 1);
+        return order[index];
+    }
+
+    string SelectReasonPhrasing()
+    {
+        int index = Random.Range(0, reason.Length - 1);
+        return reason[index];
+    }
+}
