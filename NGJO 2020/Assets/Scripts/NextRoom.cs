10c8a17f0f9d041ba4c5e3f91e3ad118385cdc62
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2020-04-18 11:01:29 +0200

    Added next room script and other rooms.

---
+++ NGJO 2020/Assets/Scripts/NextRoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextRoom : MonoBehaviour {
+	public int scenesCount = 2;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.GetComponent<PlayerMovement>()) {
+			string s;
+			do {
+				s = "Room" + Random.Range(1, scenesCount + 1);
+			} while (s == SceneManager.GetActiveScene().name);
+			SceneManager.LoadScene(s);
+		}
+	}
+}
