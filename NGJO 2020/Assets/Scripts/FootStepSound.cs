d4563afcb185c93dc4cb66deb7de6d2fa3dc8435
LasseAafeldt <superlla@hotmail.com>
2020-04-19 16:56:40 +0200

    footstep script

---
+++ NGJO 2020/Assets/Scripts/FootStepSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FootStepSound : MonoBehaviour
+{
+    public AudioClip footstepClip;
+
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayFootstep()
+    {
+        source.PlayOneShot(footstepClip);
+    }
+}
