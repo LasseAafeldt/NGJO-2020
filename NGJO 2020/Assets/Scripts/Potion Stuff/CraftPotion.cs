bdb8c84bd71e47220016b4d461756db235c73c69
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2020-04-19 12:13:14 +0200

    Changes to potion UI layout.

--- NGJO 2020/Assets/Scripts/Potion Stuff/CraftPotion.cs
+++ NGJO 2020/Assets/Scripts/Potion Stuff/CraftPotion.cs
@@ -2,36 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraftPotion : MonoBehaviour
-{
-    public Inventory inventory;
+public class CraftPotion : MonoBehaviour {
+	public GameObject allPotionsCrafted;
 
-    public void MakePotion()
-    {
-        PotionScriptableObject potionToCraft = ListOfPotions.instance.GetPotionToCraft();
-        if(inventory == null)
-        {
-            Debug.LogError("No inventory was assigned in craftPotion component");
-            return;
-        }
-        //if have enough ingredient, enable craft
-        if (!inventory.CheckIngredients(potionToCraft))
-        {
-            Debug.Log("I don't have enough ingredients");
-            return;
-        }
-        //take away ingredients
-        for (int i = 0; i < potionToCraft.ingredients.Count; i++)
-        {
-            IngredientScriptableObject ingredient = potionToCraft.ingredients[i];
-            int amountRequired = potionToCraft.amountOfIngredients[i];
-            for (int j = 0; j < amountRequired; j++)
-            {
-                inventory.RemoveIngredient(ingredient);
-                Debug.Log("Removed ingredient");
-            }
-        }
-        potionToCraft.amountOfPotion++;
-        Debug.Log("I crafted a potion");
-    }
+	public void MakePotion() {
+		PotionScriptableObject potionToCraft = ListOfPotions.instance.GetPotionToCraft();
+		if (Inventory.instance == null) {
+			Debug.LogError("No inventory was assigned in craftPotion component");
+			return;
+		}
+		//if have enough ingredient, enable craft
+		if (!Inventory.instance.CheckIngredients(potionToCraft)) {
+			Debug.Log("I don't have enough ingredients");
+			return;
+		}
+		//take away ingredients
+		for (int i = 0; i < potionToCraft.ingredients.Count; i++) {
+			IngredientScriptableObject ingredient = potionToCraft.ingredients[i];
+			int amountRequired = potionToCraft.amountOfIngredients[i];
+			for (int j = 0; j < amountRequired; j++) {
+				Inventory.instance.RemoveIngredient(ingredient);
+				Debug.Log("Removed ingredient");
+			}
+		}
+		potionToCraft.amountOfPotion--;
+		Debug.Log("I crafted a potion");
+
+		foreach (PotionScriptableObject p in Inventory.instance.potionsToCraft) {
+			if (p.amountOfPotion > 0) {
+				return;
+			}
+		}
+		allPotionsCrafted.SetActive(true);
+	}
 }
