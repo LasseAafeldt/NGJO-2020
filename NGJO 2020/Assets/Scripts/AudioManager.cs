066779ea0850cc23a586433e26481ec8caec19ec
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2020-04-19 10:31:14 +0200

    Added background music.

---
+++ NGJO 2020/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour {
+	public static AudioManager singleton;
+	AudioSource backgroundSource;
+
+	private void Awake() {
+		if (singleton == null) {
+			singleton = this;
+		} else if (singleton != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		backgroundSource = GetComponent<AudioSource>();
+	}
+
+	public void PlayBackground(AudioClip clip) {
+		if (clip != null && clip != singleton.backgroundSource.clip) {
+			singleton.backgroundSource.Stop();
+			singleton.backgroundSource.clip = clip;
+			singleton.backgroundSource.Play();
+		}
+	}
+}
